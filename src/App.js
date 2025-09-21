6140600093a789984c50c3ee55afde1bb99c62d3
Bhavesh <bhavesh.jibhakate@gmail.com>
2025-09-21 12:34:35 +0530

    removed warning errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import axios from "axios";
 import "./App.css";
-import { useState } from "react";
 import Dashboard from "./Dashboard";
-import Pagination from "./Pagination";
 
 function App() {
-  const itemsperpage = 10;
-  const [currentpage, setcurrentpage] = useState(1);
-  const [data, setData] = useState("");
-
+  
   return (
     <>
       <Dashboard />
